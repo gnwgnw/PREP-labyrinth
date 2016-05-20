44d7d77e3b30a081606eba4ab6bb0fc9bb7525b0
XfortunaX <buklin.sergei@yandex.ru>
2016-05-20 23:31:06 +0400

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -1,6 +1,5 @@
-
 //
-// Created by tsv on 09.05.16.
+// Created by bsv on 20.05.16.
 //
 
 #include "Runner.hpp"
@@ -17,7 +16,6 @@ Runner::Runner()
 void Runner::set_dir()
 {
     f_check = 1;
-    last_step = 0;
 
     if(current_status.up == BlockType::WALL && current_status.right != BlockType::WALL)
         left_dir = 1;
@@ -39,144 +37,68 @@ Direction Runner::step()
 
 Direction Runner::check_move()
 {
-    int flag = 0;
-    int flag1 = 0;
-    if(current_status.up == BlockType::WALL)
-        flag ++;
-    if(current_status.right == BlockType::WALL)
-        flag ++;
-    if(current_status.down == BlockType::WALL)
-        flag ++;
-    if(current_status.left == BlockType::WALL)
-        flag ++;
-
-    if (flag == 2)
+    if(left_dir == 1 && current_status.up != BlockType::WALL)
     {
-        if(current_status.left == BlockType::WALL && current_status.up == BlockType::WALL && last_step == 4 && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 2;
-        }
-        if(current_status.left == BlockType::WALL && current_status.up == BlockType::WALL && last_step == 1 && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 1;
-        }
-        if(current_status.right == BlockType::WALL && current_status.up == BlockType::WALL && last_step == 2  && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 2;
-        }
-        if(current_status.right == BlockType::WALL && current_status.up == BlockType::WALL && last_step == 1 && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 3;
-        }
-        if(current_status.left == BlockType::WALL && current_status.down == BlockType::WALL && last_step == 4 && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 4;
-        }
-        if(current_status.left == BlockType::WALL && current_status.down == BlockType::WALL && last_step == 3 && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 1;
-        }
-        if(current_status.right == BlockType::WALL && current_status.down == BlockType::WALL && last_step == 3  && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 3;
-        }
-        if(current_status.right == BlockType::WALL && current_status.down == BlockType::WALL && last_step == 2 && flag1 == 0)
-        {
-            flag1 = 1;
-            left_dir = 4;
-        }
-    }
-
-    if (flag == 3)
-    {
-        if(current_status.left != BlockType::WALL)
-            left_dir = 3;
-        if(current_status.right != BlockType::WALL)
-            left_dir = 1;
-        if(current_status.up != BlockType::WALL)
-            left_dir = 4;
-        if(current_status.down != BlockType::WALL)
-            left_dir = 2;
-        last_step = 0;
-    }
-
-
-    if(left_dir == 4 && flag == 1 && current_status.up != BlockType::WALL && current_status.left == BlockType::WALL)
-    {
-       last_step = 1;
-       return Direction::UP;
-    }
-    if(left_dir == 2 && flag == 1 && current_status.down != BlockType::WALL  && current_status.right == BlockType::WALL)
-    {
-       last_step = 3;
-       return Direction::DOWN;
-    }
-    if(left_dir == 1 && flag == 1 && current_status.right != BlockType::WALL  && current_status.up == BlockType::WALL)
-    {
-       last_step = 2;
-       return Direction::RIGHT;
-    }
-    if(left_dir == 3 && flag == 1 && current_status.left != BlockType::WALL  && current_status.down == BlockType::WALL)
-    {
-       last_step = 4;
-       return Direction::LEFT;
-    }
-
-    if(left_dir == 4 && flag == 1 && current_status.left != BlockType::WALL)
-    {
-       last_step = 4;
-       left_dir = 3;
-       return Direction::LEFT;
-    }
-    if(left_dir == 2 && flag == 1 && current_status.right != BlockType::WALL)
-    {
-       left_dir = 1;
-       last_step = 2;
-       return Direction::RIGHT;
-    }
-    if(left_dir == 1 && flag == 1 && current_status.up != BlockType::WALL)
-    {
-       left_dir = 4;
-       last_step = 1;
-       return Direction::UP;
-    }
-    if(left_dir == 3 && flag == 1 && current_status.down != BlockType::WALL)
-    {
-       left_dir = 2;
-       last_step = 3;
-       return Direction::DOWN;
-    }
-
-    if (flag == 0)
-    {
-        return Direction::LEFT;
-    }
-
-    if(left_dir == 1 && last_step != 4)
-    {
-        last_step = 2;
-        return Direction::RIGHT;
-    }
-    if(left_dir == 2 && last_step != 1)
-    {
-        last_step = 3;
-        return Direction::DOWN;
-    }
-    if(left_dir == 3 && last_step != 2)
-    {
-        last_step = 4;
-        return Direction::LEFT;
-    }
-    if(left_dir == 4 && last_step != 3)
-    {
-        last_step = 1;
+        left_dir = 4;
         return Direction::UP;
     }
+    if(left_dir == 2 && current_status.right != BlockType::WALL)
+    {
+        left_dir = 1;
+        return Direction::RIGHT;
+    }
+    if(left_dir == 3 && current_status.down != BlockType::WALL)
+    {
+        left_dir = 2;
+        return Direction::DOWN;
+    }
+    if(left_dir == 4 && current_status.left != BlockType::WALL)
+    {
+        left_dir = 3;
+        return Direction::LEFT;
+    }
+
+    if(left_dir == 1 && current_status.right != BlockType::WALL)
+        return Direction::RIGHT;
+    if(left_dir == 2 && current_status.down != BlockType::WALL)
+        return Direction::DOWN;
+    if(left_dir == 3 && current_status.left != BlockType::WALL)
+        return Direction::LEFT;
+    if(left_dir == 4 && current_status.up != BlockType::WALL)
+        return Direction::UP;
+
+    left_dir++;
+    if (left_dir == 5)
+        left_dir = 1;
+
+    if(left_dir == 1 && current_status.right != BlockType::WALL)
+        return Direction::RIGHT;
+    if(left_dir == 2 && current_status.down != BlockType::WALL)
+        return Direction::DOWN;
+    if(left_dir == 3 && current_status.left != BlockType::WALL)
+        return Direction::LEFT;
+    if(left_dir == 4 && current_status.up != BlockType::WALL)
+        return Direction::UP;
+
+    if(left_dir == 1)
+    {
+        left_dir = 3;
+        return Direction::LEFT;
+    }
+    if(left_dir == 2)
+    {
+        left_dir = 4;
+        return Direction::UP;
+    }
+    if(left_dir == 3)
+    {
+        left_dir = 1;
+        return Direction::RIGHT;
+    }
+    if(left_dir == 4)
+    {
+        left_dir = 2;
+        return Direction::DOWN;
+    }
+
 }

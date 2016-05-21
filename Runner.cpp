5eee33137ce0a40a9232923d9308a4a85680cd4a
Ed Asriyan <asriyan1998@yandex.ru>
2016-05-22 00:48:17 +0300

    new rand

--- Runner.cpp
+++ Runner.cpp
@@ -32,7 +32,7 @@ Direction Runner::step(){
 		history.pop();
 		isForwardDirection = false;
 	} else {
-		if (oneDirectionStepCount > 0){
+		if (oneDirectionStepCount < 100){
 			do {
 				newDirection = Direction(rand() % 4);
 			} while (c.getDirectionState(newDirection));

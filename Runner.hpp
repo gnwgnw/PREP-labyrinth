f757df32cafb557952797d8e640b5a41b6a7441d
JPYATACHKOV <CVKUCHEROV@YANDEX.RU>
2016-05-22 01:40:33 +0300

    Кирилл Кучеров, основное задание

--- Runner.hpp
+++ Runner.hpp
@@ -5,11 +5,20 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
+#include <cstdint>
+#include <cstddef>
+
 #include "RunnerBase.hpp"
 
 class Runner: public RunnerBase {
     public:
         Direction step();
+
+private:
+        Direction _prevDir = Direction::NONE;
+
+        Direction __find(const BlockType &bt);
+        size_t    __countWalls() const;
 };
 
 

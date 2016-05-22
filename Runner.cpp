6afbe452ac172fb06d5c5eddf21c9841cd067a9a
Metalluga <semrog@yandex.ru>
2016-05-22 20:27:33 +0400

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -95,12 +95,6 @@ Direction Runner::step()
     Point right = map[yIndexForMap][xIndexForMap + 1];
     Point left = map[yIndexForMap][xIndexForMap - 1];
 
-    if((current_status.left != BlockType::WALL) && (left.wave - 1 == position.wave))
-    {
-        position = left;
-        return Direction::LEFT;
-    }
-
     if((current_status.up != BlockType::WALL) && (upper.wave - 1 == position.wave))
     {
         position = upper;
@@ -117,6 +111,12 @@ Direction Runner::step()
     {
         position = lower;
         return Direction::DOWN;
+    }
+
+    if((current_status.left != BlockType::WALL) && (left.wave - 1 == position.wave))
+    {
+        position = left;
+        return Direction::LEFT;
     }
 
     if(current_status.right != BlockType::WALL && right.wave == position.wave - 1)

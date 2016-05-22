ff32ac080f0e2e1c781efd6620aaad1d5684338e
EienKotonaru <mogilnikoff2@gmail.com>
2016-05-22 20:10:07 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -1,16 +1,18 @@
-//
-// Created by tsv on 09.05.16.
-//
-
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
-
 #include "RunnerBase.hpp"
 
 class Runner: public RunnerBase {
-    public:
-        Direction step();
-};
+    private:
+        int f_check;
+        int right_dir;
 
+    public:
+        Runner();
+        Direction step();
+        Direction check_move();
+        void set_dir();
+
+};
 
 #endif //LABYRINTH_RUNNER_HPP

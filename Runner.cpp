cce9b54173b22927ba5b55abf37e9a9200e12940
Ed Asriyan <asriyan1998@yandex.ru>
2016-05-22 22:54:24 +0300

    b90k

--- Runner.cpp
+++ Runner.cpp
@@ -20,7 +20,7 @@ Direction Runner::step(){
 	Cell& c = history.top();
 	Direction newDirection;
 
-	if (history.size() > 20000){
+	if (history.size() > 90000){
 		for (auto a: dirs){
 			c.setDirectionState(a, true);
 			// std::cout << "Go back " << history.size() << '\n';

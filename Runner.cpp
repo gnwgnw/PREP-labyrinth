51161eb0b696721903ca475aed8e6b15081e6ec3
Григорий <gzharkikh@mail.ru>
2016-05-22 19:02:51 +0300

    commit

--- Runner.cpp
+++ Runner.cpp
@@ -4,16 +4,14 @@
 
 #include "Runner.hpp"
 
-#include "vector"
 #include "Runner.hpp"
 #include "iostream"
 #include "stack"
-#include "cstdlib"
-#include <ctime>
 
 
 typedef BlockType B;
 typedef Direction D;
+
 
 
 bool Runner::is_free(D d)
@@ -58,18 +56,26 @@ bool Runner::deadlock()
     int flag = 0;
 
     if (is_free(D::RIGHT)) {
+      //  direction.push_back(D::RIGHT);
+        free_right = true;
         flag ++;
     }
 
     if (is_free(D::LEFT)) {
+     //   direction.push_back(D::LEFT);
+        free_left = true;
         flag++;
     }
 
     if (is_free(D::UP)) {
+     //   direction.push_back(D::UP);
+        free_up = true;
         flag++;
     }
 
     if (is_free(D::DOWN)) {
+       // direction.push_back(D::DOWN);
+        free_down = true;
         flag++;
     }
 
@@ -85,35 +91,362 @@ D reverse(D d) {
     if (d == D::UP) return D::DOWN;
 }
 
+void Runner::clear_current_dir() {
+    free_right = false;
+    free_up = false;
+    free_down = false;
+    free_left = false;
+}
+
+
+
+std::vector<Direction >* Runner::where() {
+
+    std::vector<Direction> *v = new std::vector<Direction>();
+
+    if (is_free(D::DOWN)) {
+        v->push_back(D::DOWN);
+    }
+
+    if (is_free(D::RIGHT)) {
+        v->push_back(D::RIGHT);
+    }
+
+    if (is_free(D::UP)) {
+        v->push_back(D::UP);
+    }
+
+    if (is_free(D::LEFT)) {
+        v->push_back(D::LEFT);
+    }
+    return v;
+
+}
 
 D Runner::step() {
-
+    count++;
     if (stack.empty()) {
         Node node;
         stack.push(node);
     }
-
 
     if (deadlock()) {
         stack.pop();
         D d = reverse(stack.top().direction); // Если тупик вернуться назад
         return d;
     }
+
     else {
+
         if (current_status.left == B::EXIT) {
             return D::LEFT;
         }
+
         if (current_status.right == B::EXIT) {
             return D::RIGHT;
         }
+
         if (current_status.up == B::EXIT) {
             return D::UP;
         }
+
         if (current_status.down == B::EXIT) {
             return D::DOWN;
         }
 
-        if (is_free(D::RIGHT)) {
+        bool flag = false;
+
+
+
+      /*  std::vector<Direction > *v = where();
+
+        int n = v->size();
+        int rand_direction = rand()%n;
+
+        D dir = v->data()[rand_direction];
+
+        delete(v);
+
+        if (!flag)
+        switch (dir) {
+            case D::RIGHT: {
+                    stack.top().right = true;
+                    stack.top().direction = D::RIGHT;
+
+                    Node node;
+                    node.left = true;
+
+                    stack.push(node);
+
+                    clear_current_dir();
+
+                    stack.top().direction = D::RIGHT;
+
+
+                    return D::RIGHT;
+            }
+
+            case D::LEFT: {
+                    stack.top().left = true;
+                    stack.top().direction = D::LEFT;
+
+                    Node node;
+                    node.right = true;
+
+                    stack.push(node);
+
+                    clear_current_dir();
+
+                    stack.top().direction = D::LEFT;
+
+
+                    return D::LEFT;
+            }
+
+            case D::UP: {
+                    stack.top().up = true;
+                    stack.top().direction = D::UP;
+
+                    Node node;
+                    node.down = true;
+
+                    stack.push(node);
+
+                    clear_current_dir();
+
+                    stack.top().direction = D::UP;
+
+
+                    return D::UP;
+
+
+            }
+
+            case D::DOWN: {
+                    stack.top().down = true;
+                    stack.top().direction = D::DOWN;
+
+                    Node node;
+                    node.up = true;
+
+                    stack.push(node);
+
+                    clear_current_dir();
+
+                    stack.top().direction = D::DOWN;
+
+
+                    return D::DOWN;
+
+
+            }
+
+            }
+
+        */
+
+
+
+
+        if (count < 1400000) {
+            if ((free_right && !flag) || (free_right && flag && stack.top().direction == D::RIGHT)) {
+                stack.top().right = true;
+                stack.top().direction = D::RIGHT;
+
+                Node node;
+                node.left = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::RIGHT;
+
+
+                return D::RIGHT;
+            }
+
+            if ((free_left && !flag) || (free_left && flag && stack.top().direction == D::LEFT)) {
+                stack.top().left = true;
+                stack.top().direction = D::LEFT;
+
+                Node node;
+                node.right = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::LEFT;
+
+
+                return D::LEFT;
+            }
+
+            if ((free_down && !flag) || (free_down && flag && stack.top().direction == D::DOWN)) {
+                stack.top().down = true;
+                stack.top().direction = D::DOWN;
+
+                Node node;
+                node.up = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::DOWN;
+
+
+                return D::DOWN;
+
+            }
+
+
+            if ((free_up && !flag) || (free_up && flag && stack.top().direction == D::UP)) {
+                stack.top().up = true;
+                stack.top().direction = D::UP;
+
+                Node node;
+                node.down = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::UP;
+
+
+                return D::UP;
+
+            }
+
+
+
+
+
+        }
+        else {
+//            if (is_free(stack.top().direction)) {
+//                flag = true;
+//            }
+/*            if (is_free(stack.top().direction)) {
+                flag = true;
+            }*/
+
+
+            if ((free_down && !flag) || (free_down && flag && stack.top().direction == D::DOWN)) {
+                stack.top().down = true;
+                stack.top().direction = D::DOWN;
+
+                Node node;
+                node.up = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::DOWN;
+
+
+                return D::DOWN;
+
+            }
+
+            if ((free_right && !flag) || (free_right && flag && stack.top().direction == D::RIGHT)) {
+                stack.top().right = true;
+                stack.top().direction = D::RIGHT;
+
+                Node node;
+                node.left = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::RIGHT;
+
+
+                return D::RIGHT;
+            }
+
+
+
+            if ((free_up && !flag) || (free_up && flag && stack.top().direction == D::UP)) {
+                stack.top().up = true;
+                stack.top().direction = D::UP;
+
+                Node node;
+                node.down = true;
+
+                stack.push(node);
+
+                clear_current_dir();
+
+                stack.top().direction = D::UP;
+
+
+                return D::UP;
+
+            }
+
+
+
+        if ((free_left && !flag) || (free_left && flag && stack.top().direction == D::LEFT)) {
+            stack.top().left = true;
+            stack.top().direction = D::LEFT;
+
+            Node node;
+            node.right = true;
+
+            stack.push(node);
+
+            clear_current_dir();
+
+            stack.top().direction = D::LEFT;
+
+
+            return D::LEFT;
+        }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+       /* if ((free_down && !flag) || (free_down && flag && stack.top().direction == D::DOWN)) {
+            stack.top().down = true;
+            stack.top().direction = D::DOWN;
+
+            Node node;
+            node.up = true;
+
+            stack.push(node);
+
+            clear_current_dir();
+
+            stack.top().direction = D::DOWN;
+
+
+            return D::DOWN;
+
+        }
+
+        if ((free_right && !flag) || (free_right && flag && stack.top().direction == D::RIGHT)) {
             stack.top().right = true;
             stack.top().direction = D::RIGHT;
 
@@ -122,33 +455,20 @@ D Runner::step() {
 
             stack.push(node);
 
+            clear_current_dir();
+
+            stack.top().direction = D::RIGHT;
+
+
             return D::RIGHT;
         }
 
-        if (is_free(D::DOWN)) {
-            stack.top().down = true;
-            stack.top().direction = D::DOWN;
 
-            Node node;
-            node.up = true;
 
-            stack.push(node);
-            return D::DOWN;
 
-            }
 
-        if (is_free(D::LEFT)) {
-            stack.top().left = true;
-            stack.top().direction = D::LEFT;
 
-            Node node;
-            node.right = true;
-
-            stack.push(node);
-            return D::LEFT;
-            }
-
-        if (is_free(D::UP)) {
+        if ((free_up && !flag) || (free_up && flag && stack.top().direction == D::UP)) {
             stack.top().up = true;
             stack.top().direction = D::UP;
 
@@ -156,8 +476,34 @@ D Runner::step() {
             node.down = true;
 
             stack.push(node);
+
+            clear_current_dir();
+
+            stack.top().direction = D::UP;
+
+
             return D::UP;
 
-            }
+        }
+
+
+        if ((free_left && !flag) || (free_left && flag && stack.top().direction == D::LEFT)) {
+            stack.top().left = true;
+            stack.top().direction = D::LEFT;
+
+            Node node;
+            node.right = true;
+
+            stack.push(node);
+
+            clear_current_dir();
+
+            stack.top().direction = D::LEFT;
+
+
+            return D::LEFT;
+        }
+*/
+
         }
     }

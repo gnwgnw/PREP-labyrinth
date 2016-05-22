e2e7d3b79eb6a995ea64e6f612f6e8b699ed4c5b
Ed Asriyan <asriyan1998@yandex.ru>
2016-05-22 23:45:14 +0300

    1000

--- Runner.cpp
+++ Runner.cpp
@@ -20,7 +20,7 @@ Direction Runner::step(){
 	Cell& c = history.top();
 	Direction newDirection;
 
-	if (history.size() > 675000 || oneWayStepCount > 1400){
+	if (history.size() > 675000 || oneWayStepCount > 1000){
 		for (auto a: dirs){
 			c.setDirectionState(a, true);
 			// std::cout << "Go back " << history.size() << '\n';

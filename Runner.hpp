c007f6d22ad39c6645f7a1700961c9f8c9186701
SvetaZlobina <svetlanazlobina97@gmail.com>
2016-05-22 18:15:06 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -5,9 +5,21 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
+#include <vector>
 #include "RunnerBase.hpp"
 
+using std::vector;
+
+struct Used_Directions { //0-не ходили, 1-оттуда пришли, 2-ходили и там стена
+	int up;
+	int down;
+	int left;
+	int right;
+};
+
 class Runner: public RunnerBase {
+        size_t square_number = 0;
+        vector<Used_Directions> squares_on_field;
     public:
         Direction step();
 };

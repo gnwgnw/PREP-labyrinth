0a434d9495af8772d802c23bda5e0dc0fd2b1715
IVyazmin <ilja.vyazmin@mail.ru>
2016-05-22 22:58:11 +0300

    вариант12

--- Runner.cpp
+++ Runner.cpp
@@ -130,9 +130,9 @@ void Runner::find_directions_first()
 void Runner::find_directions_second()
 {
     find_directions_2();
-    find_directions_4();
     find_directions_1();
     find_directions_3();
+    find_directions_4();
 }
 
     void Runner::find_directions()

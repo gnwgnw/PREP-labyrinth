4a196288e1d88a1d12159c88312843a79033a679
JPYATACHKOV <CVKUCHEROV@YANDEX.RU>
2016-05-22 14:42:08 +0300

    Дефолтный  main

--- main.cpp
+++ main.cpp
@@ -21,9 +21,6 @@ int main(int argc, char* argv[])
         file.open(argv[1]);
         file >> field;
 
-        field.result(cout);
-        return 0;
-
         while (!field.is_done()) {
             field.tic();
         }
